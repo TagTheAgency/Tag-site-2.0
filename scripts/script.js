265f705e0e59004e7e60d59de12a8a851546e80c
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-12-05 16:42:46 +1300

    re-organise images folder, add personal pages using json object

--- scripts/script.js
+++ scripts/script.js
@@ -26,18 +26,34 @@ tagApp.config(function($routeProvider) {
     .when('/partners', {
         templateUrl : "../pages/partners.html"
     })
+    .when('/staff', {
+        templateUrl : "../pages/personal.html"
+    })
     .otherwise({
         redirectTo: '/'
     });
 });
 
-tagApp.controller('mainController', function($scope, $location, $window) {
+tagApp.controller('mainController', function($scope, $location, $window, dataService, $http) {
+    dataService.getStaffBios(function (response) {
+        $scope.staffObj = response.data
+    })
+
     $scope.go = function (path) {
       $location.path(path);
     };
 
     $scope.openTab = function (url) {
         $window.open(url, '_blank');
+    }
+
+    $scope.getCurrentStaffObj =function ($event) {
+        staff_id = $event.target.dataset.id
+        $.each($scope.staffObj, function(i, val) {
+          if (val.id == staff_id) {
+            $scope.current_staff = val
+          }
+        });
     }
 
     $scope.partnersObj = {
@@ -51,54 +67,17 @@ tagApp.controller('mainController', function($scope, $location, $window) {
             logo: 'https://1793699979.rsc.cdn77.org/partners/sy-engage/logo.png'
         }
     }
-    
-    $scope.staffObj = {
-        mabel: {
-            name: 'Mabel',
-            thumbnail: 'https://1793699979.rsc.cdn77.org/team-photos/mabel/1.jpg',
-            role: 'Head of Security'
-        },
-        jess: {
-            name: 'Jess Bovey',
-            thumbnail: 'https://1793699979.rsc.cdn77.org/team-photos/jess/1.jpg',
-            role: 'GM Operations'
-        },
-        jaime: {
-            name: 'Jaime Parlett',
-            thumbnail: 'https://1793699979.rsc.cdn77.org/team-photos/jaime/1.jpg',
-            role: 'Account Director'
-        },
-        rosa: {
-            name: 'Rosa Santana',
-            thumbnail: 'https://1793699979.rsc.cdn77.org/team-photos/rosa/1.jpg',
-            role: 'Designer'
-        },
-        anna: {
-            name: 'Anna Vallance',
-            thumbnail: '',
-            role: 'Account Manager'
-        },
-        madeline: {
-            name: 'Madeline Taylor',
-            thumbnail: '',
-            role: 'Designer'
-        },
-        dan: {
-            name: 'Dan Harris',
-            thumbnail: '',
-            role: 'Video Production'
-        },
-        ira: {
-            name: 'Ira Ritchie Meek',
-            thumbnail: 'https://1793699979.rsc.cdn77.org/team-photos/ira/1.jpg',
-            role: 'Developer'
-        },
-        tom: {
-            name: 'Tom Rediy',
-            thumbnail: '',
-            role: 'Head of Innovation'
-        }
-    }
-
 
 });
+
+
+
+angular.module('tagApp')
+    .service('dataService', function($http, $q) {
+        this.getStaffBios = function(cb) {
+          $http({
+              url: 'scripts/staff.json', 
+              method: "GET"
+          }).then(cb)
+        };
+    })
