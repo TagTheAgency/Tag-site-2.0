3bdcc6bf1d32bc17d1b4791cfc513b20dfad677c
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-12-23 09:45:23 +1300

    change interpolation sytnax to ','

--- scripts/mail.php
+++ scripts/mail.php
@@ -21,12 +21,12 @@ if($_POST){
 	    }
 	  ],
 	  "from": {
-	    "email": "{{$email}}"
+	    "email": "'$email'"
 	  },
 	  "content": [
 	    {
 	      "type": "text/plain",
-	      "value": "{{$email}}{{$phone}}{{$message}}"
+	      "value": "', $name, $email, $phone, $message, '"
 	    }
 	  ]
 	}');
