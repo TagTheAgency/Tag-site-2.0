414856b6a1067a644591d6dffbb0d8e71492fb09
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-12-22 15:32:29 +1300

    add sendgrid

--- scripts/mail.php
+++ scripts/mail.php
@@ -1,13 +1,35 @@
 <?php
+// If you are using Composer
+// require 'vendor/autoload.php';
 
-if($_POST){
-    $name = $_POST['form_name'];
-    $email = $_POST['form_email'];
-    $phone = $_POST['form_phone'];
-    $message = $_POST['form_msg'];
-    $headers = "From: " .$email;
+// If you are not using Composer (recommended)
+require("sendgrid-php/sendgrid-php.php");
 
-//send email
-    mail("ira@tagtheagency.com", "Contact Page Enquiry From " .$email, $message, $headers);
-}
-?>
+$request_body = json_decode('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "test@example.com"
+        }
+      ],
+      "subject": "Hello World from the SendGrid PHP Library!"
+    }
+  ],
+  "from": {
+    "email": "test@example.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, Email!"
+    }
+  ]
+}');
+
+$apiKey = getenv(process.env.SENDGRID_APIKEY);
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
