c4bc3a5fbca180f251563ab3760a00e09e3d878a
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2016-12-22 14:58:01 +1300

    fix php from header

--- scripts/mail.php
+++ scripts/mail.php
@@ -5,7 +5,7 @@ if($_POST){
     $email = $_POST['form_email'];
     $phone = $_POST['form_phone'];
     $message = $_POST['form_msg'];
-    $headers = "From: " .$email . "\r\n" .
+    $headers = "From: " .$email;
 
 //send email
     mail("ira@tagtheagency.com", "Contact Page Enquiry From " .$email, $message, $headers);
